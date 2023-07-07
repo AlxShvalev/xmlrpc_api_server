b28e77c7cb5e7152bc51463fe61e67e6ad6b0b58
Alex Shvalev <shvalevalx@gmail.com>
2023-07-07 15:58:19 +0500

    refactored server_services

--- server_services.py
+++ server_services.py
@@ -9,8 +9,8 @@ from settings import settings
 
 class ServerService:
 
-    def __init__(self, db_service: DBService) -> None:
-        self.__db_service = db_service
+    def __init__(self, service: DBService) -> None:
+        self.__db_service = service
 
     def get_user(self, username: str, password: str) -> User:
         user = self.__db_service.get_user(username)
@@ -19,6 +19,12 @@ class ServerService:
         if user.password != password:
             raise Exception("Password incorrect.")
         return user
+
+    def __session_is_expired(self, session: Session) -> Session:
+        if session.expired_date < datetime.now():
+            self.__db_service.delete_session(session)
+            raise Exception("Session is expired. Please sign in again.")
+        return session
 
     def create_session(self, user: User) -> str:
         session = self.__db_service.get_session_by_user_id(user.id)
@@ -31,20 +37,17 @@ class ServerService:
         session = self.__db_service.create_session(session)
         return str(session.id)
 
-    def __session_is_expired(self, session: Session) -> bool:
-        return True if session.expired_date < datetime.now() else False
-
     def get_partial_key(self, session_id: UUID, pub_keys: dict) -> int:
         session = self.__db_service.get_session(session_id)
-        if self.__session_is_expired(session):
-            self.__db_service.delete_session(session)
-            return "Session is expired. Please sign in again."
+        if session is None:
+            raise Exception("Session id is incorrect.")
+        session = self.__session_is_expired(session)
         try:
             session.pub_key_1 = int(pub_keys["pub_key1"])
             session.pub_key_2 = int(pub_keys["pub_key2"])
             session.partial_key_client = int(pub_keys["partial_key_client"])
         except:
-            raise Exception("Required keys are missing.")
+            raise Exception("Required public keys are missing.")
         encrypt = DHEncrypt(session.pub_key_1, session.pub_key_2, settings.DH_SECRET_KEY)
         session.partial_key_server = encrypt.generate_partial_key()
         session = self.__db_service.update_session(session)
