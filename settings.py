f42f32768906ec34a28b8d833f5b8ba4d63f16d8
Alex Shvalev <shvalevalx@gmail.com>
2023-07-12 13:22:39 +0500

    added python-dotenv

--- settings.py
+++ settings.py
@@ -1,14 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class Settings:
     """Base application settings class."""
 
-    SERVER_HOST: str = "localhost"
-    SERVER_PORT: int = 8080
+    SERVER_HOST: str = os.getenv("SERVER_HOST", "localhost")
+    SERVER_PORT: int = os.getenv("SERVER_PORT", 8080)
 
-    DB_NAME: str = "postgres"
-    DB_USER: str = "postgres"
-    DB_PASSWORD: str = "postgres"
-    DB_HOST: str = "localhost"
-    DB_PORT: str = "5432"
+    DB_NAME: str = os.getenv("DB_NAME", "postgres")
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST: str = os.getenv("DB_PORT", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
 
     SESSION_LIFETIME: int = 60 * 30 # Seconds
     CHALLENGE_MIN_LENGTH: int = 30
@@ -16,7 +23,7 @@ class Settings:
 
     PUBLIC_KEY1: int = 28491
     PUBLIC_KEY2: int = 6007
-    DH_SECRET_KEY: int = 344471
+    DH_SECRET_KEY: int = os.getenv("DH_SECRET_KEY", 221)
 
     @property
     def database_url(self) -> str:
